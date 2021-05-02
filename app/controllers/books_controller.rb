9acd84829d000ce65a829fe203e5bca27f6bc60c
fr15ky3816 <shota.t.3816@gmail.com>
2021-05-02 00:30:17 +0000

    2nd

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,10 +2,12 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
@@ -36,14 +38,14 @@ class BooksController < ApplicationController
 
   def delete
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 
 end
