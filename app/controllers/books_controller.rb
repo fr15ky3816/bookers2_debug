2651e5f6a232d48acb926004f6f787dd05459266
fr15ky3816 <shota.t.3816@gmail.com>
2021-05-02 19:29:40 +0000

    3 4th

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @book_new = Book.new
-    @post_comment = PostComment.new
+    @book_comment = BookComment.new
   end
 
   def index
--- app/models/book_comment.rb
+++ app/models/book_comment.rb
@@ -1,7 +1,6 @@
-class PostComment < ApplicationRecord
+class BookComment < ApplicationRecord
     belongs_to :book
     belongs_to :user
 
     validates :comment, presence: true
-    
 end
