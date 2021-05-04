568c65199428beb91186548a7ace4a83eabd6606
fr15ky3816 <shota.t.3816@gmail.com>
2021-05-04 00:35:46 +0000

    4 3rd

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,10 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   
   has_many :reverce_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :followers, through: :reverce_of_relationships, source: :follower_id
+  has_many :followers, through: :reverce_of_relationships, source: :follower
   
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :following, through: :relationships, source: :followed
+  has_many :followings, through: :relationships, source: :followed
   
   
   
