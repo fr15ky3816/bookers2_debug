c156c4045737879baa12fd67e61e1782d9bb0607
fr15ky3816 <shota.t.3816@gmail.com>
2021-05-10 03:13:21 +0000

    7 done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,27 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top, :about]
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :email,
+      :name,
+      :postcode,
+      :prefecture_name,
+      :address_city,
+      :address_street,
+      :address_building
+    ])
+  end
+  
+  
+  
+  
+  
+  
+  
+  
+  
   protected
   def after_sign_up_path_for(resource)
     user_path(current_user)
@@ -18,4 +39,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
+  
+  
 end
